f3bfa33933ddca043489d5f897f98f65e34806c7
Lee Isles <leeisles@gmail.com>
2020-10-15 21:45:02 -0400

    mostly working

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const got = require('got');
+const path = require('path');
+const { createWriteStream } = require('fs');
 
-request({
-  method: 'GET',
-  url: 'https://archive.org/details/attentionkmartshoppers?and[]=mediatype%3A%22audio%22'
-}, (err, res, body) => {
+(async () => {
+  try {
+    // get the index of kmart tapes
+    const indexResponse = await got('https://archive.org/details/attentionkmartshoppers');
+    let $ = cheerio.load(indexResponse.body);
+    
+    // look for all classes partially names 'item-ttl' and get the hrefs
+    // add them to an array of links, we'll go through all of these next
+    let contents = $('[class~="item-ttl"]').contents();
+    for (let i = 0; i < contents.length; i++) {
+      if (contents[i].type === 'tag' && contents[i].name === 'a') {
+        let thisURL = 'https://archive.org' + contents[i].attribs.href;
 
-  if (err) return console.error(err);
-  let $ = cheerio.load(body);
-  let links = [];
+        // scrape this resulting page for the mp3 and download it
+        const thisResponse = await got(thisURL);
+        let pageCheerio = cheerio.load(thisResponse.body);
+        let pageContents = pageCheerio('div.format-group').contents();
 
-  $('a').each(function (i, e) {
-      links[i] = $(this).attr('href');
-  });
+        for (let ii = 0; ii < pageContents.length; ii++) {
+          if (pageContents[ii].type === 'tag' 
+              && pageContents[ii].name === 'a'
+              && pageContents[ii].attribs.href.includes('mp3')) {
 
-  console.log(links);
+            let downloadLink = 'https://archive.org' + pageContents[ii].attribs.href;
+            let fileName = decodeURI(path.parse(downloadLink).base);
+            console.log('downloading', downloadLink)
+                
+            got.stream(downloadLink).pipe(createWriteStream('./mp3/'+fileName));
+          }
+        }
+      }
+    }
 
-})
+  }
+  catch (error) {
+      console.log(error.response.body);
+  }
+})();
